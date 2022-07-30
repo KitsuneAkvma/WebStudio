71a6ee343ca29d82d94e5ca86127d1e97ec7ee4b
Mateusz Martin <mateusz.martinn@gmail.com>
2022-07-30 19:41:19 +0200

    Minor fixes
    - hamburger button
    - tablet layout
    - team member bottom padding

--- public/js/mobile-menu.js
+++ public/js/mobile-menu.js
@@ -5,12 +5,14 @@ const burgerRefs = {
 
 burgerRefs.burgerBtn.addEventListener('click', () => {
   burgerRefs.burgerBtn.classList.toggle('active');
+  burgerRefs.burgerBtn.classList.toggle('fixed-btn--burger');
   burgerRefs.burgerWindow.classList.toggle('isHidden');
 });
 
 document.querySelectorAll('.burger-window__nav-list__item').forEach(n =>
   n.addEventListener('click', () => {
     burgerRefs.burgerBtn.classList.remove('active');
+    burgerRefs.burgerBtn.classList.remove('fixed-btn--burger');
     burgerRefs.burgerWindow.classList.remove('isHidden');
   })
 );
